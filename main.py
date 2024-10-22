8f0d04fdbc5a571a3bd0bbdaec773998d9a2e7c1
Fred Morrison <frederick.morrison@outlook.com>
2024-10-22 14:15:37 -0600

    add scaled simple window example

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 import sys
+import PySimpleGUI as sg
+
+from settings import Settings
 
 
 def get_python_version() -> str:
     return f'{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}'
 
+def display_simple_window() -> None:
+    layout = [
+        [sg.Text(f'Hello PySimpleGUI using Python {get_python_version()}')],
+    ]
+    settings = Settings()
+    print(f'{settings=}')
+
+    window = sg.Window('PySimpleGUI example', layout, size=(settings.scaled_width, settings.scaled_height))
+    while True:
+        event, values = window.read()
+        #if event == sg.WIN_CLOSED or event == 'Exit':
+        if event in (None, 'Exit'):
+            break
+    window.close()
+    print('end simple window demo')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     msg = f'Python version {get_python_version()}'
     print(msg)
+    display_simple_window()
