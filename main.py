11a36ed2dacd228280f8e0cc0f657c5b21754332
Fred Morrison <frederick.morrison@outlook.com>
2024-10-24 18:09:37 -0600

    add color and font information

--- main.py
+++ main.py
@@ -14,12 +14,13 @@ def get_pysimplegui_version() -> str:
 
 def display_simple_window() -> None:
     layout = [
-        [sg.Text(f'Hello PySimpleGUI using Python {get_python_version()}')],
+        [sg.Text(f'Hello PySimpleGUI using Python {get_python_version()}',
+                 text_color='white', background_color='red', font='"Courier New" 20 bold')],
     ]
-    settings = Settings(0.8)
+    settings = Settings(0.667)
     print(f'{settings=}')
     title: str = f'PySimpleGUI version {get_pysimplegui_version()}'
-    window = sg.Window(title, layout, size=(settings.scaled_width, settings.scaled_height))
+    window = sg.Window(title, layout, size=(settings.scaled_width, settings.scaled_height), resizable=True, background_color='white')
     while True:
         event, values = window.read()
         # print(f'{event==sg.WIN_CLOSED}')
@@ -32,7 +33,6 @@ def display_simple_window() -> None:
     print('end simple window demo')
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     msg = f'Python version {get_python_version()} and PySimpleGUI version {get_pysimplegui_version()}'
     print(msg)
